38183d1ce489134e22f448ff6b7d1c211a621a94
ErosPhil <eros.bts@outlook.com>
2022-12-16 12:02:33 +0100

    Commit Projet fini v1.0

--- src/controleur/FenFXML_occupationController.java
+++ src/controleur/FenFXML_occupationController.java
@@ -16,6 +16,7 @@ import modele.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 //Occupation : 
 public class FenFXML_occupationController implements Initializable {
@@ -29,7 +30,7 @@ public class FenFXML_occupationController implements Initializable {
     @FXML
     private DatePicker dateTo;
     @FXML
-    private ListView lvOccupation;
+    private ListView lvOccupations;
     @FXML
     private Button btnAfficher;
     
@@ -41,19 +42,40 @@ public class FenFXML_occupationController implements Initializable {
     
     @FXML
     public void handleBoutonAfficher(){
+        lvOccupations.getItems().clear();
         Salle maSalle = (Salle)cmbSalle.getSelectionModel().getSelectedItem();
+        
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        
         LocalDate maDateFrom = dateFrom.getValue();
+        String dateFromFormatted = dateFormatter.format(maDateFrom);
+        
         LocalDate maDateTo = dateTo.getValue();
+        String dateToFormatted = dateFormatter.format(maDateTo);
+        
         lesReservations = GestionOccupation.listeReservationsPourUneSalleEntre2Dates(maSalle.getRefSalle(), maDateFrom, maDateTo);
-        lvOccupation.getItems().add("La salle "+maSalle.getRefSalle()+" a été réservée :");
+        if(lesReservations.size() == 0){
+            lvOccupations.getItems().add("La salle n'a pas été réservée durant la période du "+dateFromFormatted+ " au "+dateToFormatted);
+        }
+        else{
+            lvOccupations.getItems().add("La salle "+maSalle.getRefSalle()+" a été réservée :");
+        }
+        
         for(int i = 0; i < lesReservations.size(); i++)
         {
             Reservation laReservation = lesReservations.get(i);
+            
             LocalDate dateR = laReservation.getDate();
+            //<>dateFormatter déjà utilisé plus haut<>
+            String dateRFormatted = dateFormatter.format(dateR);
+            
             LocalTime heureR = laReservation.getHeure();
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH");
+            String heureRFormatted = timeFormatter.format(heureR);
+            
             String asso = laReservation.getRefAsso();
-            String uneOccupation = "le "+dateR+" à "+heureR+" par l'association"+asso;
-            lvOccupation.getItems().add(uneOccupation);
+            String uneOccupation = "le "+dateRFormatted+" à "+heureRFormatted+"h par l'association "+asso;
+            lvOccupations.getItems().add(uneOccupation);
         }
     }
 }
